5d057f1045bf71e0cff985a4c7f821e0ed849757
samratrocks <samrat.shiwakoti@gmail.com>
2020-01-07 19:37:19 -0500

    add wanted level functionality on keyup

---
+++ WantedLevel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+    
+namespace WantedLevel
+{
+    public class WantedLevel : Script
+    {
+        public WantedLevel ()
+        {
+            Tick += onTick;
+            KeyDown += onKeyDown;
+            KeyUp += onKeyUp;
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.H:
+                    Game.Player.WantedLevel = 0;
+                    break;
+                case Keys.J:
+                    Game.Player.WantedLevel = 6;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
